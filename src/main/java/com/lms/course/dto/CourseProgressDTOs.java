1288157d57bf9d65abdc938c783fb21750b563fc
victorpaularony <smallypauls@gmail.com>
2025-08-06 19:42:52 +0300

    chore: update CourseProgressDTOs.java

--- src/main/java/com/lms/course/dto/CourseProgressDTOs.java
+++ src/main/java/com/lms/course/dto/CourseProgressDTOs.java
@@ -2,7 +2,9 @@ package com.lms.course.dto;
 
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,35 +20,21 @@ public class CourseProgressDTOs {
         private Long id;
 
         @NotNull(message = "Student ID is required")
+        @Min(value = 1, message = "Student ID must be a positive number.")
         private Long studentId;
+
         @NotNull(message = "Outline ID is required")
+        @Min(value = 1, message = "course ID must be a positive number.")
         private Long outlineId;
+
         @NotNull(message = "Course ID is required")
+        @Min(value = 1, message = "course Outline ID must be a positive number.")
         private Long courseId;
+
+        @NotBlank(message = "Status must not be blank.")
         private String status;
+
+        @NotNull(message = "last update must not be null.")
         private LocalDateTime lastUpdated;
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
-    public static class CourseProgressUpdateDTO {
-        @NotNull(message = "Status is required")
-        private String status;
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
-    public static class CreateProgressStatusDTO {
-        @NotNull(message = "Student ID is required")
-        private Long studentId;
-        @NotNull(message = "Outline ID is required")
-        private Long outlineId;
-        @NotNull(message = "Course ID is required")
-        private Long courseId;
-        private String status;
     }
 }
