974c394b8c69ea4eca97b8a31b00cb4271f0dd08
victorpaularony <smallypauls@gmail.com>
2025-08-07 18:42:08 +0300

    refactor(repository): update CourseSessionRepository for session changes

--- src/main/java/com/lms/course/repository/CourseSessionRepository.java
+++ src/main/java/com/lms/course/repository/CourseSessionRepository.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.lms.course.model.Course;
+// import com.lms.course.model.Course;
 import com.lms.course.model.CourseOutline;
 import com.lms.course.model.CourseSession;
-import com.lms.course.model.Student;
+// import com.lms.course.model.Student;
 
 public interface CourseSessionRepository extends JpaRepository<CourseSession, Long> {
-    List<CourseSession> findByStudentAndOutline(Student student, Course course);
-    List<CourseSession> findByStudentId(Long studentId);
+    // List<CourseSession> findByStudentAndOutline(Student student, Course course);
+    // List<CourseSession> findByStudentId(Long studentId);
     List<CourseSession> findByOutline(CourseOutline outline);
 }
